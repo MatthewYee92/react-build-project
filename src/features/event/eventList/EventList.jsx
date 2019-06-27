9c943b66e3d5e12455c4009b946d4c298bf07f42
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-06-27 08:52:03 -0700

    updated eventList

--- src/features/event/eventList/EventList.jsx
+++ src/features/event/eventList/EventList.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import EventListItem from './EventListItem'
 
+
 class EventList extends Component {
+  
   render() {
+
     const {events} = this.props;
+
     return (
       <div>
         <h1>Event List</h1>
-        {events.map((event) => (
-          <EventListItem key={event.id} event={event}/>
+        {events.map((event) => ( {event.id} event={event}/>
         ))}
       </div>
     )
